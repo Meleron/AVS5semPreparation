d0246aa94df5d26db88bda78debc21f261062db4
Meleron <daniiltrotski487@gmail.com>
2019-12-31 15:28:15 +0300

    Fixed bug with randomized variants output

--- AVS5/Question.cs
+++ AVS5/Question.cs
@@ -9,17 +9,18 @@ namespace AVS5
     {
         public string Text { get; set; }
         public string Variants { get; set; }
-        public int ChosenAnswer { get; set; }
+        public int ChosenAnswer { get; set; } = 0;
         public int RightAnswer { get; set; }
         public bool IsRight { get; set; } = false;
+        public bool IsRandomized { get; set; } = false;
 
         public override string ToString()
         {
             StringBuilder sb = new StringBuilder();
-            if (!Program.RANDOMIZEANSWERS)
+            sb.AppendLine(Text);
+            if (!Program.RANDOMIZEANSWERS || IsRandomized)
             {
                 //  Do not randomize possible answers
-                sb.AppendLine(Text);
                 foreach (string s in Variants.Split(';'))
                     sb.AppendLine(s.Trim());
                 return sb.ToString();
@@ -29,10 +30,15 @@ namespace AVS5
                 //  Randomize possible answers
                 string[] splittedVariants = Variants.Split(';').Where(s => s != "").Select(s => s).ToArray();
                 string rightAnswerText = splittedVariants[RightAnswer - 1];
-                RightAnswer = Array.IndexOf(splittedVariants, rightAnswerText) + 1;
                 splittedVariants = new List<string>(splittedVariants).Shuffle().ToArray();
+                RightAnswer = Array.IndexOf(splittedVariants, rightAnswerText) + 1;
+                Variants = "";
                 for (int i = 0; i < splittedVariants.Length; i++)
+                {
                     sb.AppendLine($"{i + 1}) {splittedVariants[i].Trim().Substring(3)}");
+                    Variants += string.Format($"{ i + 1}) { splittedVariants[i].Trim().Substring(3)};");
+                }
+                IsRandomized = true;
                 return sb.ToString();
             }
         }
